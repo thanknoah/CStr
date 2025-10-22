414b92f85ca9adda9241ce6e31470d1733a6400b
Noah <78211701+thanknoah@users.noreply.github.com>
2025-10-22 22:49:59 +0100

    Update main.c

--- main.c
+++ main.c
@@ -11,22 +11,27 @@ typedef struct StrObj {
 	size_t size;
 	bool memoryEfficency;
 
+} StrObj;
+
+typedef struct StrLib {
+	void (*get_input)(StrObj*, size_t);
 	void (*destroy)(StrObj*);
 	void (*assign)(StrObj*, const char*);
 	void (*append)(StrObj*, const char*);
-	bool (*cmp)(StrObj*, const char*);
-} StrObj;
+	bool (*cmp)(const char*, const char*);
+} StrLib;
+
 
 size_t performance_growth(size_t currentCapacity) {
 	size_t predicted;
-	if (currentCapacity < 4 << 10) { 
-		predicted = currentCapacity << 1;   
+	if (currentCapacity < 4 << 10) {
+		predicted = currentCapacity << 1;
 	}
-	else if (currentCapacity < 4 << 20) { 
+	else if (currentCapacity < 4 << 20) {
 		predicted = currentCapacity + (currentCapacity >> 2) + (currentCapacity >> 1);
 	}
 	else if (currentCapacity < 512 << 20) {
-		predicted = currentCapacity + (currentCapacity >> 1); 
+		predicted = currentCapacity + (currentCapacity >> 1);
 	}
 	else {
 		predicted = currentCapacity + (currentCapacity >> 2);
@@ -37,16 +42,16 @@ size_t performance_growth(size_t currentCapacity) {
 
 size_t memory_efficient_growth(size_t currentCapacity) {
 	size_t predicted;
-	if (currentCapacity < 4 << 10) {  
+	if (currentCapacity < 4 << 10) {
 		predicted = currentCapacity << 1;
 	}
-	else if (currentCapacity < 1 << 20) {         
-		predicted = currentCapacity + (currentCapacity >> 1); 
+	else if (currentCapacity < 1 << 20) {
+		predicted = currentCapacity + (currentCapacity >> 1);
 	}
-	else if (currentCapacity < 100 << 20) {      
-		predicted = currentCapacity + (currentCapacity >> 3); 
+	else if (currentCapacity < 100 << 20) {
+		predicted = currentCapacity + (currentCapacity >> 3);
 	}
-	else {                                        
+	else {
 		predicted = currentCapacity + (currentCapacity >> 4);
 	}
 
@@ -62,8 +67,41 @@ void delete_string(StrObj* currentStr) {
 	currentStr->size = 0;
 }
 
-bool equal_string(StrObj* currentStr, const char* newStr) {
-	return strcmp(currentStr->data, newStr) == 0;
+bool equal_string(const char* firstStr, const char* secondStr) {
+	return strcmp(firstStr, secondStr) == 0;
+}
+
+bool input_custom_str_mem(StrObj* currentStr, size_t size) {
+	char* newMemoryBlock = (char*)malloc(size + 1);
+	if (!newMemoryBlock) {
+		printf("\nCStr Error: memory allocation failed\n");
+		return false;
+	}
+
+	free(currentStr->data);
+	currentStr->data = newMemoryBlock;
+	currentStr->capacity = size + 1;
+
+	return true;
+}
+
+
+void recv_input(StrObj* currentStr, size_t charLength) {
+	bool allocatedMemory = input_custom_str_mem(currentStr, charLength);
+	if (!allocatedMemory) {
+		printf("\nCStr Error: failed to get input");
+		return;
+	}
+
+	fgets(currentStr->data, charLength, stdin);
+	size_t size = strlen(currentStr->data);
+
+	if (currentStr->data[size - 1] == '\n' && size > 0) {
+		currentStr->data[size - 1] = '\0';
+		size--;
+	}
+
+	currentStr->size = size;
 }
 
 void modify_string(StrObj* currentStr, const char* newStr) {
@@ -78,9 +116,9 @@ void modify_string(StrObj* currentStr, const char* newStr) {
 		else
 			predictedNewSize = performance_growth(currentStr->capacity);
 
-		if (newStrSize > predictedNewSize) 
+		if (newStrSize > predictedNewSize)
 			newSize = newStrSize;
-		else 
+		else
 			newSize = predictedNewSize;
 
 
@@ -132,17 +170,21 @@ void append_string(StrObj* currentStr, const char* newStr) {
 	currentStr->size += newStrLen;
 }
 
-
-StrObj create_str(StrObj* e) {
-	e->data = (char*)malloc(15);
-	e->memoryEfficency = false;
-	e->capacity = 15;
-	e->size = 0;
-
+StrLib init_str_lib(StrLib* e) {
 	e->destroy = delete_string;
 	e->append = append_string;
 	e->assign = modify_string;
 	e->cmp = equal_string;
+	e->get_input = recv_input;
+
+	return *e;
+}
+
+StrObj init_str(StrObj* e) {
+	e->data = (char*)malloc(15);
+	e->memoryEfficency = false;
+	e->capacity = 15;
+	e->size = 0;
 
 	if (!e->data) {
 		printf("\nCStr Error: memory allocation failed\n");
@@ -155,28 +197,56 @@ StrObj create_str(StrObj* e) {
 	e->data[0] = '\0';
 	return *e;
 }
+// EXAMPLE PROGRAM
 
 int main() {
-	// Functions
-	StrObj s = create_str(&s);
-	StrObj a = create_str(&a);
+	// Initiate Str Lib
+	StrLib strlib;
+	init_str_lib(&strlib);
 
-	s.assign(&s, "Hello"); a.assign(&a, "Bye"); // Assigns value
-	s.append(&s, " World!"); a.append(&a, " World!"); // Appends value
+	// Initate Strings
+	StrObj user = init_str(&user);
+	StrObj pass = init_str(&pass);
+	StrObj message = init_str(&message);
 
-	s.cmp(&s, "Hello World!"); // Compare with strings returns true
-	s.cmp(&s, "Bye World!"); // Compare with strings returns false
-	s.cmp(&s, a.data); // compare with other StrObj (a) returns false
+	/*
+	You can also do it this way
+	StrObj user;
+	StrObj pass;
+	StrObj message;
 
-	// Print Data
-	printf("String data: ");
-	printf(s.data);
-	printf("\nSize of string: %zu", s.size);
-	printf("\nCapacity of string: %zu", s.capacity);
+	init_str(&user);
+	init_str(&pass);
+	init_str(&message);
+	*/
+
+	// Assigning str to value
+	strlib.assign(&message, "You have");
+
+	// Get Inputs
+	printf("User: ");
+	strlib.get_input(&user, 200);
+	printf("Password: ");
+	strlib.get_input(&pass, 200);
+
+	// Check if they match
+	if (strlib.cmp(user.data, "Noah") && strlib.cmp(pass.data, "MySecretPassword")) {
+		strlib.append(&message, " successfuly logged in!"); // Appending to string
+	}
+	else {
+		strlib.assign(&message, " successfuly NOT logged in!"); // Appending to string
+	}
+
+	// Print result
+	printf("\n");
+	printf(message.data);
 
 
-	s.memoryEfficency = true; // Optional, but leaving it to false is recommended for peformance
-	s.destroy(&s); // Destroy String
+	// Additional Data
+	message.size; // length of str
+	message.capacity; // size of memory block
+	message.memoryEfficency; // u can set this to true or false but recommended to be false for peformance
+
 
 	return 0;
 }
